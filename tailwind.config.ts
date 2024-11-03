71a178445a7d610a0b86b9ee96575d3b9df0117b
fugitivexyz <fugitivelabxyz@gmail.com>
2024-11-03 16:47:46 +0000

    fix: update styling configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
     darkMode: ["class"],
@@ -17,6 +18,10 @@ const config = {
             },
         },
         extend: {
+            fontFamily: {
+                sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+                mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+            },
             colors: {
                 border: "hsl(var(--border))",
                 input: "hsl(var(--input))",
